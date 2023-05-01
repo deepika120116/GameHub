16998080f4f8e50461c8f0c76bc773ca2a038c81
Deepika Somasundaram <sdeepikas88@gmail.com>
2023-05-01 13:22:07 -0400

    Responsive webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import './App.css'
+
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
- 
-
   return (
-    <Button colorScheme='blue'>Button</Button>
-  )
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}
+      gridTemplateColumns={'30vw'}
+    >
+      <GridItem area="nav" bg="coral" width='100' h='10'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        {/* greater than 1024Px, implement this */}
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      
+      <GridItem area="main" bg="skyblue">
+        Main
+      </GridItem>
+    </Grid >
+  );
 }
 
-export default App
+export default App;
