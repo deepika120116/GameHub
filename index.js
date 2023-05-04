b2d4c43f4ad24007722fd9a4e3956728fd966804
Deepika Somasundaram <sdeepikas88@gmail.com>
2023-05-04 10:47:16 -0400

    Added GameCard by replacing the unordered List

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-const stoneWeight = (stones) => {
-    while (stones.length > 1) {
-        stones.sort((a, b) => b - a);
-        const x = stones.shift();
-        const y = stones.shift();
-        if (x !== y) 
-            stones.push(x - y);
-    }
-    return (stones.length===1)?stones[0]:0;
-};
-
-console.log(lastStoneWeight([2, 7, 4, 1, 8, 1]));
-
-
-function lastStoneWeight(stones) {
-  while (stones.length > 1) {
-    stones.sort((a, b) => b - a); // sort stones in descending order
-    const y = stones.shift(); // remove the heaviest stone
-    const x = stones.shift(); // remove the second heaviest stone
-    if (x !== y) {
-      stones.push(y - x); // add the difference back to the array
-    }
-  }
-  return stones.length === 1 ? stones[0] : 0;
+const lastWordLength = (str) => {
+    const strAsArr = str.split(" ").reverse();
+    const result = strAsArr[0].length;
+    return result;
 }
+console.log(lastWordLength("i am strong and happy"));
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from 'axios';
-
-interface Game{
+ export interface Game{
     id: number;
-    name: String;
-}
+     name: String;
+     background_image: string;
+};
 interface FetchGamesResponse{
     count: number;
     results: Game[];
-}
+};
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
     useEffect(() => {
         const controller = new AbortController();
-        apiClient.get<FetchGamesResponse>('/games',{signal:controller.signal})
+        apiClient.get<FetchGamesResponse>("/games", { signal: controller.signal })
             .then(res => setGames(res.data.results))
             .catch(err => {
                 if (err instanceof CanceledError) return;
-                setError(err.message)
+                setError(err.message);
             });
         return () => controller.abort();
     }, []);
