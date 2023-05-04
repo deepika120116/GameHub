12d534a0a89bc823c75d518ea3a72fdbbbe87340
Deepika Somasundaram <sdeepikas88@gmail.com>
2023-05-04 19:46:43 -0400

    Created Platform icon list for each game card

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const lastWordLength = (str) => {
-    const strAsArr = str.split(" ").reverse();
-    const result = strAsArr[0].length;
+const spellsPotions = (spells, potions, max) => {
+    let result = [];
+    let output = [];
+    let occurence = 0;
+    for (let potion of potions) {
+        for (let spell of spells) {
+            let multiplied = spell * potion;
+           result.push( (multiplied > 7) ? occurence++ : 0);
+      }
+    
+    }
     return result;
-}
-console.log(lastWordLength("i am strong and happy"));
+};
+console.log(spellsPotions([5, 1, 3], [1, 2, 3, 4, 5]),7);
